ee7896e6435d0536e0011f9d48755b467b24f87a
pjhyoun01 <pjhyoun01@gmail.com>
2026-03-23 03:36:13 +0900

    260320 HW update

--- frontend/javascript/jQuery/HW/260320_hw/jquery.js
+++ frontend/javascript/jQuery/HW/260320_hw/jquery.js
@@ -1,25 +1,38 @@
 $().ready(function () {
     var itemNum = 0;
+    var item = [];
 
     $(".header__add").on("click", function () {
         var addDiv = $("<div>");
-        if (itemNum < 10) {
-            addDiv.text("아이템 목록 " + ++itemNum);
+        var addItem = $("<div>");
+        var deleteBtn = $("<div>");
+        deleteBtn.text("X");
+
+        if (item.length < 10) {
+            item.push(itemNum);
+            addItem.text("아이템 목록 " + ++itemNum);
+            addDiv.append(addItem);
+            addDiv.append(deleteBtn);
             $(".content").append(addDiv);
         } else {
             alert("더 이상 추가할 수 없습니다.")
         }
+        console.log(item);
 
-        $(".footer").text("총 " + itemNum + "개의 아이템이 등록되었습니다");
+        $(".footer").text("총 " + item.length + "개의 아이템이 등록되었습니다");
     });
     $(".header__removeall").on("click", function () {
-        if (!itemNum) {
+        if (!item.length) {
             alert("이미 모든 아이템이 제거되었습니다.");
             return;
         }
         $(".content > div").remove();
-        itemNum = 0;
-        $(".footer").text("총 " + itemNum + "개의 아이템이 등록되었습니다");
+
+        item = [];
+        console.log(item);
+
+        $(".footer").text("총 " + item.length + "개의 아이템이 등록되었습니다");
+        
     });
-    $(".footer").text("총 " + itemNum + "개의 아이템이 등록되었습니다");
+    $(".footer").text("총 " + item.length + "개의 아이템이 등록되었습니다");
 })
